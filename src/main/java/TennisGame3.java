2da67038681879cd4eabfdf188579ece807340d5
Cristian <camilo.santamaria322@gmail.com>
2021-06-09 23:33:14 -0500

    Version 1.0.0 Refactorizacion de Kata tenis

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -5,6 +5,7 @@ public class TennisGame3 implements TennisGame {
     private int p1;
     private String p1N;
     private String p2N;
+    private String p [] = {"Love","Fifteen","Thirty","Forty"};
 
     public TennisGame3(String p1N, String p2N) {
         this.p1N = p1N;
@@ -15,44 +16,40 @@ public class TennisGame3 implements TennisGame {
         String s;
 
         //Intento 1
-        String p [] = {"Love","Fifteen","Thirty","Forty"};
-        boolean bol1 = testGame1();
-        boolean bol2=testGame2();
-        boolean bol3=testGame3();
+        boolean bol1 = isLessFourP1();
+        boolean bol2= isLessFourP2();
+        boolean bol3= isDiferenceSix();
 
         if(bol1 && bol2 && bol3){
             s = p[p1];
-            return validateOne(s, p);
+            return messageAll(s);
         }else{
-            if (p1 == p2)
+            if (isEqualsNumbers(p1,p2))
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return validateTwo(s);
+            s = isGreaterNumber(p1,p2) ? p1N : p2N;
+            return advantageAndWin(s);
         }
     }
 
-    private String validateTwo(String s) {
-        String testing2 = ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
-        return testing2;
+    private String advantageAndWin(String s) {
+        String resAndWin = (isEqualsNumbers(multiplication(p1-p2,p1-p2),1) ? "Advantage " + s : "Win for " + s;
+        return resAndWin;
     }
 
-    private String validateOne(String s, String[] p) {
-        String testing =(p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        return testing;
+    private String messageAll(String s) {
+        String resAll =( isEqualsNumbers(p1,p2)) ? s + "-All" : s + "-" + p[p2];
+        return resAll;
     }
 
-    boolean testGame1 (){
-        return (p1 < 4);
+    boolean isLessFourP1(){
+        return (islessNumbers(p1,4));
     }
-
-    boolean testGame2(){
-        return(p2 < 4);
+    boolean isLessFourP2(){
+        return(islessNumbers(p2,4));
     }
-
-    boolean testGame3(){
-        return (!(p1 + p2 == 6));
+    boolean isDiferenceSix(){
+        return (!(isEqualsNumbers(plus(p1,p2),6)));
     }
-
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
@@ -61,5 +58,20 @@ public class TennisGame3 implements TennisGame {
             this.p2 += 1;
         
     }
+
+    private int plus(int number1, int number2){return number1+number2;}
+
+    private int multiplication(int number1, int number2){return number1*number2;}
+
+    private boolean isGreaterNumber(int number1, int number2) {
+        return number1 >=number2;
+    }
+    private boolean isEqualsNumbers(int number1, int number2) {
+        return number1 ==number2;
+    }
+    private boolean islessNumbers(int number1, int number2) {
+        return number1 < number2;
+    }
+
 
 }
