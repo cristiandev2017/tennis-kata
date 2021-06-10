fd5f69e1bf6ac5c31d5bc326ab7a5f1726281763
Cristian <camilo.santamaria322@gmail.com>
2021-06-09 21:52:48 -0500

    Se agrega refactorizacion del TennisGame2 se bajo a 29, se realizara mas refactorizacion inicialmente fue proceso de extract methods

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,6 +1,7 @@
 
 public class TennisGame2 implements TennisGame
 {
+    //Se puede refactorizar con una clase para acortar variables
     public int P1point = 0;
     public int P2point = 0;
 
@@ -8,6 +9,8 @@ public class TennisGame2 implements TennisGame
     public String P2res = "";
     private String player1Name;
     private String player2Name;
+    private String palabras[] = {"Fifteen","Thirty","Forty"};
+    private String score = "";
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -15,91 +18,135 @@ public class TennisGame2 implements TennisGame
     }
 
     public String getScore(){
-        String score = "";
-        if (P1point == P2point && P1point < 4)
+        if (isEqualsNumbers(P1point,P2point)  &&  isLessNumber(P1point, 4))
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+            score = validateEqualsScore(score);
         }
-        if (P1point==P2point && P1point>=3)
+        if (isEqualsNumbers(P1point,P2point) && isGreaterThanNumber(P1point,3))
             score = "Deuce";
 
-        if (P1point > 0 && P2point==0)
+        if (isGreaterNumber(P1point,0) && isEqualsNumbers(P2point,0))
         {
+
+            P1res = validatePoints(P1point);
+            /*
             if (P1point==1)
                 P1res = "Fifteen";
             if (P1point==2)
                 P1res = "Thirty";
             if (P1point==3)
                 P1res = "Forty";
-
+            */
             P2res = "Love";
             score = P1res + "-" + P2res;
         }
         if (P2point > 0 && P1point==0)
         {
+
+            P2res = validatePoints(P2point);
+            /*
             if (P2point==1)
                 P2res = "Fifteen";
             if (P2point==2)
                 P2res = "Thirty";
             if (P2point==3)
                 P2res = "Forty";
-
+            */
             P1res = "Love";
             score = P1res + "-" + P2res;
         }
 
-        if (P1point>P2point && P1point < 4)
+        if (isGreaterNumber(P1point,P2point) && isLessNumber(P1point,4))
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+            validatePointsFour();
+
         }
-        if (P2point>P1point && P2point < 4)
+        if (isGreaterNumber(P2point,P1point)&& isLessNumber(P2point,4))
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+            validatePointsFourP2();
         }
 
-        if (P1point > P2point && P2point >= 3)
+        if (isGreaterNumber(P1point,P2point)  && isGreaterThanNumber(P2point,3) )
         {
             score = "Advantage player1";
         }
 
-        if (P2point > P1point && P1point >= 3)
+        if (isGreaterNumber(P2point,P1point) && isGreaterThanNumber(P1point,3))
         {
             score = "Advantage player2";
         }
 
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+        if (isGreaterThanNumber(P1point,4) && isGreaterThanNumber(P2point,0) && isGreaterThanNumber (P1point-P2point,2))
         {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        if (isGreaterThanNumber(P2point,4) && isGreaterThanNumber(P1point,0)  && isGreaterThanNumber(P2point-P1point,2))
         {
             score = "Win for player2";
         }
         return score;
     }
 
+    private void validatePointsFourP2() {
+        if (P2point==2)
+            P2res="Thirty";
+        if (P2point==3)
+            P2res="Forty";
+        if (P1point==1)
+            P1res="Fifteen";
+        if (P1point==2)
+            P1res="Thirty";
+        score = P1res + "-" + P2res;
+    }
+
+    private void validatePointsFour() {
+        if (isEqualsNumbers(P1point,2))
+            P1res="Thirty";
+        if (isEqualsNumbers(P1point,3))
+            P1res="Forty";
+        if (isEqualsNumbers(P2point,1))
+            P2res="Fifteen";
+        if (isEqualsNumbers(P2point,2))
+            P2res="Thirty";
+        score = P1res + "-" + P2res;
+    }
+
+    private String validatePoints(int number) {
+        if(isGreaterThanNumber(number,0) && isLessNumber(number,4)){
+            //P1res= palabras[number-1];
+            return palabras[number-1];
+        }
+        return "";
+    }
+
+    private String validateEqualsScore(String score) {
+        if (isEqualsNumbers(P1point,0))
+            score = "Love";
+        if (isEqualsNumbers(P1point,1))
+            score = "Fifteen";
+        if (isEqualsNumbers(P1point,2))
+            score = "Thirty";
+        score += "-All";
+        return score;
+    }
+
+    private boolean isGreaterThanNumber(int number1, int number2) {
+        return number1 >=number2;
+    }
+
+    private boolean isGreaterNumber(int number1, int number2) {
+        return number1 >number2;
+    }
+
+    private boolean isLessNumber(int number1, int number2) {
+        return number1 <number2;
+    }
+
+    private boolean isEqualsNumbers(int number1, int number2) {
+        return number1 ==number2;
+    }
+
+    //Reutilizar metodos se estan usando la misma funcionalidad
     public void SetP1Score(int number){
 
         for (int i = 0; i < number; i++)
@@ -118,6 +165,7 @@ public class TennisGame2 implements TennisGame
 
     }
 
+    //Reutilizar una sola función enviar el P1point como parametro
     public void P1Score(){
         P1point++;
     }
